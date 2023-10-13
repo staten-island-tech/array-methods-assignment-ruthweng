f9daa889ba7868c59ce606f4811521afdb0c7f25
ruthweng <ruthweng333@gmail.com>
2023-10-13 13:15:30 -0400

    2

--- objects.js
+++ objects.js
@@ -2,26 +2,45 @@ const students = [
     {
         firstName: "Bernese",
         lastName: "Lie",
-        graduated: false;
+        failing: true,
         DOB: 1800,
-        siblings: ["Ceedy"], 
+        siblings: ["Rose"], 
         age: function () {
                 return year - this.DOB;
             }, 
+            num: [1, 2, 3]
         },
     
         {
             firstName: "Joannie",
             lastName: "O'Chen",
-            graduated: false;
+            failing: false,
             DOB: 2003,
             siblings: ["Peeka"], 
             age: function () {
                     return year - this.DOB;
                 }, 
+                num: [4, 5, 6]
+            },
+
+            {
+                firstName: "Jujuboob",
+                lastName: "Huang",
+                failing: false,
+                DOB: 2005,
+                siblings: ["King Andrew"],
+                age: function () {
+                    return year - this.DOB;
+                },
+                num: [7, 8, 9]
             },
     ];
     
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    //console.log(num(num.length - 1));
+    //foreach is a for loop
+
+    //num.forEach(num, index) => console.log(num, index);
     numbers.forEach((number, index) => console.log(number, index));
     students.forEach((student)=> console.log(student.firstName));
     
